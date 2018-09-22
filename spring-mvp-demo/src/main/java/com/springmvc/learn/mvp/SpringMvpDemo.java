9a0b3a0087fca3670fc404cbbd9381377bd99bec
qiankai02 <qiankai02@ppdai.com>
2018-09-22 13:37:25 +0800

    调试修改

--- spring-mvp-demo/src/main/java/com/springmvc/learn/mvp/SpringMvpDemo.java
+++ spring-mvp-demo/src/main/java/com/springmvc/learn/mvp/SpringMvpDemo.java
@@ -4,20 +4,21 @@ import com.springmvc.learn.mvp.bean.IVehicle;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- */
 public class SpringMvpDemo {
     public static void main(String[] args) {
         springWarmUp();
     }
 
     protected static void springWarmUp() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
-        IVehicle tortoise = (IVehicle) applicationContext.getBean("tortoise");
-        tortoise.run();
 
-        IVehicle rabbit = (IVehicle)applicationContext.getBean("rabbit");
-        rabbit.run();
+        //解析上下文文件，创建其中定义的bean
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+
+        //获取并使用其中的bean
+        IVehicle bike = (IVehicle) applicationContext.getBean("bike");
+        bike.run();
+
+        IVehicle car = (IVehicle)applicationContext.getBean("car");
+        car.run();
     }
 }
